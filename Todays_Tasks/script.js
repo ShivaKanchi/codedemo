e3de2ee6eea0be16a6bca8ba2d0ed5e6888713dd
ShivaKanchi <shivakanchi111@gmail.com>
2024-02-10 17:58:51 +0530

    New Html

---
+++ Todays_Tasks/script.js
@@ -0,0 +1,55 @@
+const time = new Date();
+const todaysDayEle = document.querySelector(".task__todaysDate");
+todaysDayEle.textContent = time.toDateString();
+
+const hourlytasksWrapper = document.getElementById("taskList");
+
+const totalHours = 24;
+const startHour = 7;
+const endHour = 23;
+const allTodaysTask = [];
+
+function addHours() {
+  // Append Hours to Task List
+  for (let i = startHour; i <= endHour; i++) {
+    const hourTask = document.createElement("div");
+    hourTask.classList.add("task__hour");
+    hourTask.dataset.hour = i;
+    const hour = document.createElement("span");
+    hour.textContent = (i > 12 ? i : i) + (i >= 13 ? "pm" : "am");
+    hourTask.appendChild(hour);
+    allTodaysTask.push(hourTask);
+    hourlytasksWrapper.appendChild(hourTask);
+  }
+}
+
+function getCurrentHour() {
+  const currentHour = document.querySelector(
+    `[data-hour="${time.getHours()}"]`
+  );
+  const currentCursor = document.createElement("div");
+  currentCursor.classList.add("time__nowCursor");
+  currentHour.appendChild(currentCursor);
+  return [currentHour, currentCursor];
+}
+
+function getCurrentHour() {
+  const currentHour = document.querySelector(
+    `[data-hour="${time.getHours()}"]`
+  );
+  const currentCursor = document.createElement("div");
+  currentCursor.classList.add("time__nowCursor");
+  currentHour.appendChild(currentCursor);
+  return [currentHour, currentCursor];
+}
+
+function updateCursor(cursorEle) {
+  cursorEle.style.top = time.getMinutes() + "%";
+  return cursorEle;
+}
+
+addHours();
+let currentActiveTask = getCurrentHour();
+let updatedCursor = updateCursor(currentActiveTask[1]);
+
+console.log("update", updateCursor);
